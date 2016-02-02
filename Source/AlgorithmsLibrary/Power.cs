620252aed4c74bc124be2b2a05143fbc315bfb80
Tania Shatilova <tatashat@gmail.com>
2016-02-02 23:04:35 +0300

    + power

---
+++ Source/AlgorithmsLibrary/Power.cs
@@ -0,0 +1,26 @@
+﻿namespace AlgorithmsLibrary
+{
+    public class Power
+    {
+        public static int Power2(int a, int n)
+        {
+            int b = 1;
+            for (int i = n; i > 0; i--)
+            {
+                b = b * a;
+            }
+            return b;
+        }
+
+        public static int Power1(int a, int n)
+        {
+            int b = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                b = b * a;
+            }
+            return b;
+        }
+
+    }
+}

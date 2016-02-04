21bc63e3322d0c81565b07356c08d4ab09eb235f
Tania Shatilova <tatashat@gmail.com>
2016-02-04 23:04:54 +0300

    + Multiplication

---
+++ Source/AlgorithmsLibrary/Multiplication.cs
@@ -0,0 +1,15 @@
+﻿namespace AlgorithmsLibrary
+{
+    public class Multiplication
+    {
+        public static int DoMultiplication(int a, int b)
+        {
+            int c = 0;
+            for (int i = 0; i < b; i++)
+            {
+                c = c + a;
+            }
+            return c;
+        }
+    }
+}

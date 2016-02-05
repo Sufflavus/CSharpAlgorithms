24a0ff217b2f0b81d760a94faeaa727d29505bc9
Tania Shatilova <tatashat@gmail.com>
2016-02-05 22:22:31 +0300

    + sum

---
+++ Source/AlgorithmsLibrary/Summ.cs
@@ -0,0 +1,15 @@
+﻿namespace AlgorithmsLibrary
+{
+    public class Summ
+    {
+        public int DoSumm(int a, int b)
+        {
+            int c = a;
+            for (int i = 1; i <= b; i++)
+            {
+                c = a + i;
+            }
+            return c;
+        }
+    }
+}

f8ed9d190cb4073c7cd2371712820cdc8bf78948
Tania Shatilova <tatashat@gmail.com>
2016-02-06 22:13:00 +0300

    + division

---
+++ Source/AlgorithmsLibrary/Division.cs
@@ -0,0 +1,19 @@
+﻿using AlgorithmsLibrary.ResultData;
+
+namespace AlgorithmsLibrary
+{
+    public class Division
+    {
+        public DivisionResult DoDivision(int a, int d)
+        {
+            int r = a;
+            int q = 0;
+            while (r >= d)
+            {
+                r = r - d;
+                q++;
+            }
+            return new DivisionResult { Div = q, Mod = r };
+        }
+    }
+}
---
+++ Source/AlgorithmsLibrary/ResultData/DivisionResult.cs
@@ -0,0 +1,8 @@
+﻿namespace AlgorithmsLibrary.ResultData
+{
+    public class DivisionResult
+    {
+        public int Div { get; set; }
+        public int Mod { get; set; }
+    }
+}

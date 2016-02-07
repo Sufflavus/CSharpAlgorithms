131bf3357c0f912bb85a1fd03d63c355ed3f530c
Tania Shatilova <tatashat@gmail.com>
2016-02-07 21:12:48 +0300

    + factorial

---
+++ Source/AlgorithmsLibrary/Factorial.cs
@@ -0,0 +1,35 @@
+﻿namespace AlgorithmsLibrary
+{
+    public class Factorial
+    {
+        public int Method1(int n)
+        {
+            int result = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                result = result * i;
+            }
+            return result;
+        }
+
+       public int Method2(int n)
+        {
+            var factorials = new int[n + 1];
+            factorials[0] = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                factorials[i] = factorials[i - 1] * i;
+            }
+            return factorials[n];
+        }
+
+        public int Method3(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            return Method3(n - 1) * n;
+        }
+    }
+}

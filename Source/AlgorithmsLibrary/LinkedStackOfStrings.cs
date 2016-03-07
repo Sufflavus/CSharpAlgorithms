1da2c309ab70462d1c5e33563a924411bf51450f
Tania Shatilova <tatashat@gmail.com>
2016-03-07 23:44:33 +0300

    + linked stack

---
+++ Source/AlgorithmsLibrary/LinkedStackOfStrings.cs
@@ -0,0 +1,35 @@
+﻿namespace AlgorithmsLibrary
+{
+    public class LinkedStackOfStrings
+    {
+        private Node _first;
+
+        public void Push(string item)
+        {
+            var oldFirst = _first;
+            _first = new Node
+            {
+                Item = item,
+                Next = oldFirst
+            };
+        }
+
+        public string Pop()
+        {
+            var item = _first.Item;
+            _first = _first.Next;
+            return item;
+        }
+
+        public bool IsEmpty()
+        {
+            return _first == null;
+        }
+
+        private class Node
+        {
+            public string Item { get; set; }
+            public Node Next { get; set; }
+        }
+    }
+}

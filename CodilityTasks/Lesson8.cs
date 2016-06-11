53c4b37230f3daec41a0cf0e0439f5bf9af8efa2
Tania Shatilova <tatashat@gmail.com>
2016-06-11 23:25:02 +0300

    + lesson 8

---
+++ CodilityTasks/Lesson8.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace CodilityTasks
+{
+    public static class Lesson8
+    {
+        /// <summary>
+        /// The leader of this sequence is the element whose value occurs more than n/2 times.
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public static int SlowFindLeader(int[] array)
+        {
+            int n = array.Length;
+            int leader = -1;
+            for (int k = 0; k < n; k++)
+            {
+                int candidate = array[k];
+                int count = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    if (array[i] == candidate)
+                    {
+                        count++;
+                    }
+                }
+                if (count > n/2)
+                {
+                    leader = candidate;
+                }
+            }
+            return leader;
+        }
+
+        /// <summary>
+        /// The leader of this sequence is the element whose value occurs more than n/2 times.
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public static int FastFindLeader(int[] array)
+        {
+            int n = array.Length;
+            if (n == 0)
+            {
+                return -1;
+            }
+
+            int leader = -1;
+            int[] sortedArray = array.OrderBy(x => x).ToArray();
+            int candidate = sortedArray[n/2];
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (sortedArray[i] == candidate)
+                {
+                    count++;
+                }
+            }
+            if (count > n / 2)
+            {
+                leader = candidate;
+            }
+            return leader;
+        }
+
+        /// <summary>
+        /// The leader of this sequence is the element whose value occurs more than n/2 times.
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public static int GoldenFindLeader(int[] array)
+        {
+            int n = array.Length;
+            if (n == 0)
+            {
+                return -1;
+            }
+
+            int size = 0;
+            int value = array[0];
+            for (int k = 0; k < n; k++)
+            {
+                if (size == 0)
+                {
+                    size++;
+                    value = array[k];
+                }
+                else
+                {
+                    if (value != array[k])
+                    {
+                        size--;
+                    }
+                    else
+                    {
+                        size++;
+                    }
+                }
+            }
+
+            int candidate = -1;
+            if (size > 0)
+            {
+                candidate = value;
+            }
+            
+            int leader = -1;
+            int count = 0;
+            
+            for (int k = 0; k < n; k++)
+            {
+                if (array[k] == candidate)
+                {
+                    count++;
+                }
+            }
+            if (count > n / 2)
+            {
+                leader = candidate;
+            }
+            return leader;
+        }
+    }
+}
---
+++ Source/Tests/CodilityTasks/Lesson8Tests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using CodilityTasks;
+using Xunit;
+
+namespace Tests.CodilityTasks
+{
+    public class Lesson8Tests
+    {
+        [Theory]
+        [InlineData(new [] { 6, 8, 4, 6, 8, 6, 6 }, 6)]
+        [InlineData(new [] { 6, 8, 6, 8, 6, 8, 6, 8 }, -1)]
+        [InlineData(new [] { 6, 8, 6, 8, 6, 8, 6, 8, 8 }, 8)]
+        [InlineData(new [] { 1, 0, 3, 7, 9 }, -1)]
+        [InlineData(new int[] {}, -1)]
+        public void SlowFindLeader_CorrectResult(int[] array, int expected)
+        {
+            int result = Lesson8.SlowFindLeader(array);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(new[] { 6, 8, 4, 6, 8, 6, 6 }, 6)]
+        [InlineData(new[] { 6, 8, 6, 8, 6, 8, 6, 8 }, -1)]
+        [InlineData(new[] { 6, 8, 6, 8, 6, 8, 6, 8, 8 }, 8)]
+        [InlineData(new[] { 1, 0, 3, 7, 9 }, -1)]
+        [InlineData(new int[] { }, -1)]
+        public void FastFindLeader_CorrectResult(int[] array, int expected)
+        {
+            int result = Lesson8.FastFindLeader(array);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(new[] { 6, 8, 4, 6, 8, 6, 6 }, 6)]
+        [InlineData(new[] { 6, 8, 6, 8, 6, 8, 6, 8 }, -1)]
+        [InlineData(new[] { 6, 8, 6, 8, 6, 8, 6, 8, 8 }, 8)]
+        [InlineData(new[] { 1, 0, 3, 7, 9 }, -1)]
+        [InlineData(new int[] { }, -1)]
+        public void GoldenFindLeader_CorrectResult(int[] array, int expected)
+        {
+            int result = Lesson8.GoldenFindLeader(array);
+            Assert.Equal(expected, result);
+        }
+    }
+}

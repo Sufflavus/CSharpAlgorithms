145610f1f21f32929ca7693796104dc6bc87760c
Tania Shatilova <tatashat@gmail.com>
2016-06-11 19:34:44 +0300

    + Lesson 4

---
+++ CodilityTasks/Lesson4.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Linq;
+
+namespace CodilityTasks
+{
+    public static class Lesson4
+    {
+        /// <summary>
+        /// Count elements' occurrences. Only arrays with positive numbers.
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="m">Max value in array.</param>
+        /// <returns></returns>
+        public static int[] CountingOccurrences(int[] array, int m)
+        {
+            int[] counts = new int[m + 1];
+            foreach (int t in array)
+            {
+                counts[t] += 1;
+            }
+            return counts;
+        }
+
+        /// <summary>
+        /// The goal is to check whether there is a swap operation which can be performed on these
+        /// arrays in such a way that the sum of elements in array A equals the sum of elements in
+        /// array B after the swap.By swap operation we mean picking one element from array A and
+        /// one element from array B and exchanging them.
+        /// </summary>
+        /// <param name="arrayA"></param>
+        /// <param name="arrayB"></param>
+        /// <returns></returns>
+        public static bool SwapSlowSolution(int[] arrayA, int[] arrayB)
+        {
+            if (arrayA.Length != arrayB.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(arrayA));
+            }
+
+            int n = arrayA.Length;
+            Int64 sumA = Sum(arrayA);
+            Int64 sumB = Sum(arrayB);
+            
+            for (int i = 0; i<n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    int change = arrayB[j] - arrayA[i];
+                    sumA += change;
+                    sumB -= change;
+                    if (sumA == sumB)
+                    {
+                        return true;
+                    }
+
+                    sumA -= change;
+                    sumB += change;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// The goal is to check whether there is a swap operation which can be performed on these
+        /// arrays in such a way that the sum of elements in array A equals the sum of elements in
+        /// array B after the swap.By swap operation we mean picking one element from array A and
+        /// one element from array B and exchanging them.
+        /// </summary>
+        /// <param name="arrayA"></param>
+        /// <param name="arrayB"></param>
+        /// <param name="m">Max value in array.</param>
+        /// <returns></returns>
+        public static bool SwapFastSolution(int[] arrayA, int[] arrayB, int m)
+        {
+            if (arrayA.Length != arrayB.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(arrayA));
+            }
+
+            int n = arrayA.Length;
+            Int64 sumA = Sum(arrayA);
+            Int64 sumB = Sum(arrayB);
+
+            Int64 d = sumB - sumA;
+
+            if (d%2 == 1)
+            {
+                return false;
+            }
+
+            d /= 2;
+            int[] counts = CountingOccurrences(arrayA, m);
+
+            for (int i = 0; i < n; i++)
+            {
+                if (0 <= arrayB[i] - d && arrayB[i] - d <= m && counts[arrayB[i] - d] > 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private static Int64 Sum(int[] array)
+        {
+            return array.Sum(x => (Int64) x);
+        }
+    }
+}
---
+++ Source/Tests/CodilityTasks/Lesson4Tests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Security.Authentication;
+using CodilityTasks;
+using Xunit;
+
+namespace Tests.CodilityTasks
+{
+    public class Lesson4Tests
+    {
+        [Theory]
+        [InlineData(new[] { 0, 0, 4, 2, 4, 5 }, 5, new[] { 2, 0, 1, 0, 2, 1 })]
+        public void CountingOccurrences_PositiveArray_CorrectResult(int[] array, int m, int[] expected)
+        {
+            var result = Lesson4.CountingOccurrences(array, m);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(new[] { 0, 0, 4, 2, 4, 5 }, new[] { 2, 0, 1, 0, 2, 1 }, false)]
+        [InlineData(new[] { 1, 2, 3, 4, 5 }, new[] { 1, 3, 2, 4, 5 }, true)]
+        public void SwapSlowSolution_CorrectResult(int[] arrayA, int[] arrayB, bool expected)
+        {
+            var result = Lesson4.SwapSlowSolution(arrayA, arrayB);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(new[] { 0, 0, 4, 2, 4 }, new[] { 2, 0, 1, 0, 2, 1 })]
+        [InlineData(new[] { 1, 2, 3, 4, 5 }, new[] { 1, 3, 2, 4 })]
+        public void SwapSlowSolution_WrongArrays_Throws_ArgumentOutOfRangeException(int[] arrayA, int[] arrayB)
+        {
+            Exception ex = Assert.Throws<ArgumentOutOfRangeException>(() => Lesson4.SwapSlowSolution(arrayA, arrayB));
+        }
+
+        [Theory]
+        [InlineData(new[] { 2, 0, 1, 0, 2, 1 }, new[] { 0, 0, 4, 2, 4, 5 }, 5, false)]
+        [InlineData(new[] { 1, 2, 3, 4, 5 }, new[] { 1, 3, 2, 4, 5 }, 5, true)]
+        public void SwapFastSolution_CorrectResult(int[] arrayA, int[] arrayB, int m, bool expected)
+        {
+            var result = Lesson4.SwapFastSolution(arrayA, arrayB, m);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(new[] { 0, 0, 4, 2, 4 }, new[] { 2, 0, 1, 0, 2, 1 }, 5)]
+        [InlineData(new[] { 1, 2, 3, 4, 5 }, new[] { 1, 3, 2, 4 }, 5)]
+        public void SwapFastSolution_WrongArrays_Throws_ArgumentOutOfRangeException(int[] arrayA, int[] arrayB, int m)
+        {
+            Exception ex = Assert.Throws<ArgumentOutOfRangeException>(() => Lesson4.SwapFastSolution(arrayA, arrayB, m));
+        }
+    }
+}

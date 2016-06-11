2ce3023954763cf59330f4372084ea7895225e9c
Tania Shatilova <tatashat@gmail.com>
2016-06-11 23:04:57 +0300

    + lesson 7

---
+++ CodilityTasks/Lesson7.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CodilityTasks
+{
+    public static class Lesson7
+    {
+        /// <summary>
+        /// You are given a zero-indexed array A consisting of n integers: a[0], a[1], ... , a[n−1].
+        /// Array A represents a scenario in a grocery store, and contains only 0s and/or 1s:
+        ///• 0 represents the action of a new person joining the line in the grocery store,
+        ///• 1 represents the action of the person at the front of the queue being served and leaving
+        /// the line.
+        /// The goal is to count the minimum number of people who should have been in the line before
+        /// the above scenario, so that the scenario is possible (it is not possible to serve a person if the
+        /// line is empty).
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public static int GroceryStore(byte[] array)
+        {
+            int n = array.Length;
+            int size = 0;
+            int result = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (array[i] == 0)
+                {
+                    size++;
+                }
+                else
+                {
+                    size--;
+                    result = Math.Max(result, -size);
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Source/Tests/CodilityTasks/Lesson7Tests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using CodilityTasks;
+using Xunit;
+
+namespace Tests.CodilityTasks
+{
+    public class Lesson7Tests
+    {
+        [Theory]
+        [InlineData(new byte[] { 0, 0, 1, 0, 1, 1, 0, 0 }, 0)]
+        [InlineData(new byte[] { 1, 1, 1, 0, 1, 1, 0, 1, 1 }, 5)]
+        public void GroceryStore_CorrectResult(byte[] array, int expected)
+        {
+            int result = Lesson7.GroceryStore(array);
+            Assert.Equal(expected, result);
+        }
+    }
+}

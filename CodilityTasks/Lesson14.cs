32920d8d6e431c51ea5f33c41907ea91f694b5b9
Tania Shatilova <tatashat@gmail.com>
2016-06-12 17:24:19 +0300

    + lesson 14

---
+++ CodilityTasks/Lesson14.cs
@@ -0,0 +1,83 @@
+﻿namespace CodilityTasks
+{
+    /// <summary>
+    /// Binary search algorithm
+    /// </summary>
+    public static class Lesson14
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="array">sorted array</param>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public static int BinarySearch(int[] array, int x)
+        {
+            int n = array.Length;
+            int beg = 0;
+            int end = n - 1;
+            int result = -1;
+            while (beg <= end)
+            {
+                int mid = (beg + end)/2;
+                if (array[mid] <= x)
+                {
+                    beg = mid + 1;
+                    result = mid;
+                }
+                else
+                {
+                    end = mid - 1;
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// You are given n binary values x[0], x[1], ... , x[n−1], such that x[i] ∈ {0, 1}. This array
+        /// represents holes in a roof(1 is a hole). You are also given k boards of the same size.The goal
+        /// is to choose the optimal (minimal) size of the boards that allows all the holes to be covered
+        /// by boards.
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public static double Boards(double[] array, int k)
+        {
+            int n = array.Length;
+            int beg = 0;
+            int end = n;
+            int result = -1;
+            while (beg <= end)
+            {
+                int mid = (beg + end)/2;
+                if (Check(array, mid) <= k)
+                {
+                    end = mid - 1;
+                    result = mid;
+                }
+                else
+                {
+                    beg = mid + 1;
+                }
+            }
+            return result;
+        }
+
+        private static int Check(double[] array, int k)
+        {
+            int n = array.Length;
+            int boards = 0;
+            int last = -1;
+            for (int i = 0; i < n; i++)
+            {
+                if (array[i] == 1 && last < i)
+                {
+                    boards++;
+                    last = i + k - 1;
+                }
+            }
+            return boards;
+        }
+    }
+}
---
+++ Source/Tests/CodilityTasks/Lesson14Tests.cs
@@ -0,0 +1,25 @@
+﻿using CodilityTasks;
+using Xunit;
+
+namespace Tests.CodilityTasks
+{
+    public class Lesson14Tests
+    {
+        [Theory]
+        [InlineData(new[] { 12, 15, 15, 19, 24, 31, 53, 59, 60 }, 31, 5)]
+        [InlineData(new[] { 12, 15, 15, 19, 24, 31, 53, 59, 60 }, 20, 3)]
+        public void BinarySearch_CorrectResult(int[] array, int x, int expected)
+        {
+            int result = Lesson14.BinarySearch(array, x);
+            Assert.Equal(expected, result);
+        }
+
+        /*[Theory]
+        [InlineData(new double[] { 0.12, 0.15, 0.15, 0.19, 0.24, 0.31, 0.53, 0.59, 0.60 }, 5, 5)]
+        public void Boards_CorrectResult(double[] array, int k, double expected)
+        {
+            double result = Lesson14.Boards(array, k);
+            Assert.Equal(expected, result);
+        }*/
+    }
+}

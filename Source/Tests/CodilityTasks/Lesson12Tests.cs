51110c77dfd8490176f03151590452aa2c334177
Tania Shatilova <tatashat@gmail.com>
2016-06-12 11:12:23 +0300

    +lesson 12

---
+++ Source/Tests/CodilityTasks/Lesson12Tests.cs
@@ -0,0 +1,38 @@
+﻿using CodilityTasks;
+using Xunit;
+
+namespace Tests.CodilityTasks
+{
+    public class Lesson12Tests
+    {
+        [Theory]
+        [InlineData(10, 15, 5)]
+        [InlineData(13, 15, 1)]
+        [InlineData(20, 20, 20)]
+        public void Gcd1_CorrectResult(int a, int b, int expected)
+        {
+            int result = Lesson12.Gcd1(a, b);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(10, 15, 5)]
+        [InlineData(13, 15, 1)]
+        [InlineData(20, 20, 20)]
+        public void Gcd2_CorrectResult(int a, int b, int expected)
+        {
+            int result = Lesson12.Gcd2(a, b);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(10, 15, 5)]
+        [InlineData(13, 15, 1)]
+        [InlineData(20, 20, 20)]
+        public void Gcd3_CorrectResult(int a, int b, int expected)
+        {
+            int result = Lesson12.Gcd3(a, b, 1);
+            Assert.Equal(expected, result);
+        }
+    }
+}

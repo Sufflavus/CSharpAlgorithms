826141520fa0d9f0e425403e26d9ebbd7b24f07f
Tania Shatilova <tatashat@gmail.com>
2016-06-12 18:39:48 +0300

    + lesson 16

---
+++ CodilityTasks/Lesson16.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace CodilityTasks
+{
+    public static class Lesson16
+    {
+        /// <summary>
+        /// There are n > 0 canoeists with weights ordered enc.
+        /// The goal is to seat them in the minimum number of double canoes whose displacement(the
+        /// maximum load) equals k.You may assume that w[i] less than k.
+        /// </summary>
+        /// <param name="w"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public static int GreedyCanoeistB(int[] w, int k)
+        {
+            int canoes = 0;
+            int j = 0;
+            int i = w.Length - 1;
+            while (i >= j)
+            {
+                if (w[i] + w[j] <= k)
+                {
+                    j++;
+                }
+                canoes++;
+                i--;
+            }
+            return canoes;
+        }
+    }
+}
---
+++ Source/Tests/CodilityTasks/Lesson16Tests.cs
@@ -0,0 +1,16 @@
+﻿using CodilityTasks;
+using Xunit;
+
+namespace Tests.CodilityTasks
+{
+    public class Lesson16Tests
+    {
+        [Theory]
+        [InlineData(new[] {1, 3, 4, 5, 6, 9, 11}, 13, 4)]
+        public void GreedyCanoeistB_CorrectResult(int[] w, int k, int expected)
+        {
+            int result = Lesson16.GreedyCanoeistB(w, k);
+            Assert.Equal(expected, result);
+        }
+    }
+}

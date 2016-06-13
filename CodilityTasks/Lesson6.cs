f02d01d1e1b6c8e54e79352b11bc86841e68b435
Tania Shatilova <tatashat@gmail.com>
2016-06-13 21:12:10 +0300

    + test tasks for lessons 6

--- CodilityTasks/Lesson6.cs
+++ CodilityTasks/Lesson6.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace CodilityTasks
@@ -72,6 +73,77 @@ namespace CodilityTasks
                 }
             }
             return result;
+        }
+
+        /// <summary>
+        /// given a zero-indexed array A consisting of N integers, returns 1 if there exists a triangular triplet 
+        /// for this array and returns 0 otherwise
+        /// </summary>
+        /// <param name="array">N is an integer within the range [0..100,000];
+        /// each element of array A is an integer within the range[−2, 147, 483, 648..2, 147, 483, 647]</param>
+        /// <returns></returns>
+        public static int Triangle(int[] array)
+        {
+            int n = array.Length;
+            if (n < 3)
+            {
+                return 0;
+            }
+            int[] sortedArray = array.OrderBy(x => x).ToArray();
+            for (int i = 0; i < n - 2; i++)
+            {
+                // Beware of overflow
+                if (sortedArray[i] > 0 && sortedArray[i] > sortedArray[i + 2] - sortedArray[i + 1])
+                {
+                    return 1;
+                }
+            }
+            return 0;
+        }
+
+        /// <summary>
+        /// given a zero-indexed array A consisting of N integers, returns the number of distinct values in array A
+        /// </summary>
+        /// <param name="array">N is an integer within the range [0..100,000];
+        /// each element of array A is an integer within the range[−1, 000, 000..1, 000, 000]</param>
+        /// <returns></returns>
+        public static int Distinct(int[] array)
+        {
+            int n = array.Length;
+            if (n == 0)
+            {
+                return 0;
+            }
+
+            var sortedArray = array.OrderBy(x => x).ToArray();
+            int result = 1;
+            for (int k = 1; k < n; k++)
+            {
+                if (sortedArray[k] != sortedArray[k - 1])
+                {
+                    result++;
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// given a non-empty zero-indexed array A, returns the value of the maximal product of any triplet
+        /// </summary>
+        /// <param name="array">N is an integer within the range [3..100,000];
+        /// each element of array A is an integer within the range[−1, 000..1, 000]</param>
+        /// <returns></returns>
+        public static int MaxProductOfThree(int[] array)
+        {
+            int n = array.Length;
+            if (n < 3)
+            {
+                return 0;
+            }
+
+            int[] sortedArray = array.OrderBy(x => x).ToArray();
+            return Math.Max(sortedArray[n - 1] * sortedArray[n - 2] * sortedArray[n - 3],
+                sortedArray[0] * sortedArray[1] * sortedArray[n - 1]);
         }
     }
 }
--- Source/Tests/CodilityTasks/Lesson6Tests.cs
+++ Source/Tests/CodilityTasks/Lesson6Tests.cs
@@ -45,5 +45,60 @@ namespace Tests.CodilityTasks
             int expected = array.Distinct().Count();
             Assert.Equal(expected, result);
         }
+
+        [Theory]
+        [InlineData(new[] { 10, 2, 5, 1, 8, 20 }, 1)]
+        [InlineData(new[] { 10, 50, 5, 1 }, 0)]
+        [InlineData(new[] { 10, 50 }, 0)]
+        [InlineData(new[] { 20, 60, 70 }, 1)]
+        [InlineData(new[] { 10 }, 0)]
+        [InlineData(new[] { -10 }, 0)]
+        [InlineData(new[] { 10, -50 }, 0)]
+        [InlineData(new[] { 10, 2, -5, 1, -8, 20 }, 0)]
+        [InlineData(new[] { 0, 0, 0, 0, 0 }, 0)]
+        [InlineData(new[] { -10, -2, -5, -1, -8, -20 }, 0)]
+        [InlineData(new int[] { }, 0)]
+        public void Triangle_CorrectResult(int[] array, int expected)
+        {
+            int result = Lesson6.Triangle(array);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(new[] { 1, 8, 4, 2, 4, 5 })]
+        [InlineData(new[] { 2, 1, 1, 2, 3, 1 })]
+        [InlineData(new[] { 1 })]
+        [InlineData(new[] { -1000000, 1000000 })]
+        [InlineData(new[] { -1, -1, -5, 0, 8, -8 })]
+        [InlineData(new[] { 1, 3, 2, 7, 1, 0 })]
+        [InlineData(new[] { 1, 3, 3, 7, 9, 3, 8, 9, 8, 1, 3 })]
+        [InlineData(new int[0])]
+        public void Distinct_CorrectResult(int[] array)
+        {
+            int result = Lesson6.DistinctCount(array);
+
+            int expected = array.Distinct().Count();
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(new[] { 1, 8, 4, 2, 4, 5 }, 160)]
+        [InlineData(new[] { 2, 1, 1, 2, 3, 1 }, 12)]
+        [InlineData(new[] { 1 }, 0)]
+        [InlineData(new[] { 1, 2 }, 0)]
+        [InlineData(new[] { 1, 2, 3 }, 6)]
+        [InlineData(new[] { -1, -2, -3 }, -6)]
+        [InlineData(new[] { -3, 1, 2, -2, 5, 6 }, 60)]
+        [InlineData(new[] { -1, -3, 2, 5 }, 15)]
+        [InlineData(new[] { -1000, 999, 1000 }, -999000000)]
+        [InlineData(new[] { -1, -1, -5, 0, 8, -8 }, 320)]
+        [InlineData(new[] { 1, 3, 2, 7, 1, 0 }, 42)]
+        [InlineData(new[] { 1, 3, 3, 7, 9, 3, 8, 9, 8, 1, 3 }, 648)]
+        [InlineData(new int[0], 0)]
+        public void MaxProductOfThree_CorrectResult(int[] array, int expected)
+        {
+            int result = Lesson6.MaxProductOfThree(array);
+            Assert.Equal(expected, result);
+        }
     }
 }

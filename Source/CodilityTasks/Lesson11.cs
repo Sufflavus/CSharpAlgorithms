a54315e2be49119b011c48a650849f3fbc46e238
Tania Shatilova <tatashat@gmail.com>
2016-06-28 23:06:11 +0300

    + CountSemiprimes

--- Source/CodilityTasks/Lesson11.cs
+++ Source/CodilityTasks/Lesson11.cs
@@ -54,6 +54,38 @@ namespace CodilityTasks
             return primeFactors.ToArray();
         }
 
+        public static int[] CountSemiprimes(int n, int[] p, int[] q)
+        {
+            bool[] sieve = Sieve(n);
+            int[] counts = new int[n];
+
+            counts[0] = 0;
+            counts[1] = 0;
+            counts[2] = 0;
+            counts[3] = 0;
+            counts[4] = 1;
+
+            int m = 2;
+
+            for (int i = 5; i < n; i++)
+            {
+                counts[i] = m;
+                if (sieve[i])
+                {
+                    m++;
+                }
+            }
+
+            var result = new List<int>();
+
+            for (int i = 0; i < q.Length; i++)
+            {
+                result.Add(counts[q[i]] - counts[p[i] - 1]);
+            }
+
+            return result.ToArray();
+        }
+
         private static int[] ArrayF(int n)
         {
             int[] f = new int[n + 1];

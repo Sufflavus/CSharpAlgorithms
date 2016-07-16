8b621f8eeef96ce682284a544a7d732e7d43fb1a
Tania Shatilova <tatashat@gmail.com>
2016-07-17 00:14:22 +0400

    + TieRopes tests

--- Source/Tests/CodilityTasks/Lesson16Tests.cs
+++ Source/Tests/CodilityTasks/Lesson16Tests.cs
@@ -12,5 +12,14 @@ namespace Tests.CodilityTasks
             int result = Lesson16.GreedyCanoeistB(w, k);
             Assert.Equal(expected, result);
         }
+
+        [Theory]
+        [InlineData(4, new[] { 1, 2, 3, 4, 1, 1, 3 }, 3)]
+        [InlineData(1, new[] { 1 }, 1)]
+        public void TieRopes_CorrectResult(int k, int[] array, int expected)
+        {
+            int result = Lesson16.TieRopes(k, array);
+            Assert.Equal(expected, result);
+        }
     }
 }
